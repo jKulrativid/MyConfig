be1453eabcbeb927bbf45611d489cf23a59edb1b
jkulrativid <jetnipit.kul@blockfint.com>
2024-11-23 14:42:45 +0700

    update shiftwidth and tab size

--- nvim/lua/jetnipit/core/options.lua
+++ nvim/lua/jetnipit/core/options.lua
@@ -6,8 +6,8 @@ opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tab
-opt.shiftwidth = 2 -- 2 space for indent width
+opt.tabstop = 4    -- 4 spaces for tab
+opt.shiftwidth = 4 -- 4 space for indent width
 opt.expandtab = true
 opt.autoindent = true
 
@@ -21,8 +21,8 @@ opt.cursorline = true
 
 -- terminal integration
 opt.termguicolors = true -- turn on termguicolors
-opt.background = "dark" -- colorschemes that can be light or dark will be dark
-opt.signcolumn = "yes"  -- show the sign column so that text doesn't shift
+opt.background = "dark"  -- colorschemes that can be light or dark will be dark
+opt.signcolumn = "yes"   -- show the sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line, or insert mode start position
@@ -33,5 +33,3 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 -- split window
 opt.splitright = true -- split vertical window on the right
 opt.splitbelow = true -- split horizontal window on the bottom
-
-
